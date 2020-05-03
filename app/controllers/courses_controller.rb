f549ca4199b6a43a2c00681544d5b7be41a1baa0
Beedeeboom <stephanie.duffieux@gmail.com>
2020-05-03 18:18:41 +1000

    Initial commit

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,39 @@
+class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.create(params.require(:course).permit(:name))
+    if @course.save
+      redirect_to courses_path
+    else
+      flash[:error] = @course.errors.full_messages
+      redirect_to_new_course_path
+    end
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    @course.update(params.require(:course).permit.(:name))
+    redirect_to course_path(@course)
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+    redirect_to courses_path
+  end
+end
